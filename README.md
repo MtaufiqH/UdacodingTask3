c57d758b9a5e6a40f52066002bf84e90efa892ba
M. Taufiq Hidayat <mtaufiqh62@gmail.com>
2020-11-13 22:16:11 +0800

    menyelesaikan Page Batik

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Explore Indonesiaku
 
+
+
 ## About App
-    Project ini merupakan portofolio submission Udacoding Mentoring yang diwajibkan get data dari internet dengan beberapa API yang telah disiapkan oleh facilitator
+Project ini merupakan portofolio submission Udacoding Mentoring yang diwajibkan get data dari internet dengan beberapa API yang telah disiapkan oleh facilitator
+
 
 ## Android Studio as your main IDE
    The recommended IDE for Android development is Android Studio because it is developed and constantly updated by Google, has good support for Gradle, contains a range of useful monitoring and analysis tools and is fully tailored for Android development.
--- app/src/main/java/id/taufiq/udacodingtask3/view/BatikDetail.kt
+++ app/src/main/java/id/taufiq/udacodingtask3/view/BatikDetail.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import coil.load
 import id.taufiq.udacodingtask3.R
 import kotlinx.android.synthetic.main.fragment_batik_detail.*
+import java.text.NumberFormat
+import java.util.*
 
 
 class BatikDetail : Fragment() {
@@ -24,6 +27,25 @@ class BatikDetail : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        iv_detail_batik.load(args.BatikData.link_batik) {
+            this.crossfade(true)
+                .placeholder(R.drawable.ic_baseline_image_24)
+        }
+        with(args.BatikData) {
+            tv_nama_batik_detail.text = nama_batik
+            tv_daerah_batik.text = daerah_batik
+            tv_makna_batik.text = makna_batik
+
+            // lokalisasi format rupiah
+            val locale = Locale("id", "ID")
+            val formatRupiah = NumberFormat.getCurrencyInstance(locale)
+            tv_harga_tinggi.text = formatRupiah.format(harga_tinggi.toDouble())
+            tv_harga_rendah.text = formatRupiah.format(harga_rendah.toDouble())
+
+
+        }
 
 
     }
